fba4f37b9131be40cb61126a1771d60ab89acb7a
ayush_ahirwar-12 <ayushahirwar04@gmail.com>
2025-11-08 16:00:14 +0530

    g

--- module pattern/js1.js
+++ module pattern/js1.js
@@ -1,36 +1,33 @@
-class youtubeChannel {
-  constructor() {
-    this.subscribers = [];
-  }
-  subscribe(user) {
-    this.subscribers.push(user);
-    user.update(`You have subscribed the channel`);
-  }
-  unsubscribe(user){
-    this.subscribers = this.subscribers.filter((sub)=>sub!==user)
-    user.update(`you have unsubscribed the channel`)
-  }
-  notify(msg){
-    this.subscribers.forEach((sub)=>sub.update(msg));
-  }
+class youtubeChannel{
+    constructor(){
+        this.subscribers = []
+    }
+    subscribe(user){
+        this.subscribers.push(user)
+        user.update("you have subscribed the channel")
+    }
+    unsubscribe(user){
+        this.subscribers=this.subscribers.filter((sub)=>sub!==user)
+        user.update("you have unsubscribed the channel")
+
+    }
+    notify(msg){
+        this.subscribers.forEach((sub)=>sub.update(msg))
+    }
 }
 
-class user {
-  constructor(name) {
-    this.name = name;
-  }
-  update(data) {
-    console.log(`${this.name} ${data}`);
-  }
+class User{
+    constructor(name){
+        this.name=name;
+    }
+    update(data){
+        console.log(`${this.name},${data}`);
+        
+    }
 }
 
 let sheryians = new youtubeChannel();
-let user1 = new user("ayush");
-let user2= new user("harsh")
+let user1 = new User("ayush");
 
 sheryians.subscribe(user1)
-sheryians.subscribe(user2)
-
-sheryians.notify("New video is now uploaded")
-sheryians.unsubscribe(user2)
-sheryians.notify("We are going live tonight")
+sheryians.notify("WE will go live tonight")
